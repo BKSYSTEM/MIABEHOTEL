97dee30a3fbad859205ab20896be665f8abe3f34
BKSYSTEM <fbonkey22@gmail.com>
2023-03-24 01:23:54 +0000

    Update de la page de login

--- lib/authenticate/login_page.dart
+++ lib/authenticate/login_page.dart
@@ -1,166 +1,267 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:miabehotel/authenticate/forgot_page.dart';
 import 'package:miabehotel/constants/colors.dart';
 import 'package:miabehotel/screens/custom_backgroound.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  final VoidCallback showRegisterPage;
+  const LoginPage({super.key, required this.showRegisterPage});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final keys = GlobalKey<FormState>();
+
+  //text controller
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: CustomBackground(
           child: Center(
-            child: Container(
-              padding: const EdgeInsets.only(
-                top: 50,
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    'Bon retour !',
-                    style: GoogleFonts.nunito(
-                      color: blackColor,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    'Content de vous revoir',
-                    style: GoogleFonts.nunito(
-                      color: greyColor,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Image.asset(
-                    'assets/images/welcome.png',
-                  ),
-                  const SizedBox(height: 10),
-                  Form(
-                    child: Column(
-                      children: [],
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  MaterialButton(
-                    onPressed: null,
-                    child: Container(
-                      color: primaryColor,
-                      margin: const EdgeInsets.symmetric(horizontal: 10),
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      width: MediaQuery.of(context).size.width,
-                      child: Text(
-                        "Se connecter",
-                        style: GoogleFonts.nunito(
-                          color: whiteColor,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w800,
-                        ),
-                        textAlign: TextAlign.center,
+            child: SingleChildScrollView(
+              child: Form(
+                key: keys,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Bon retour !',
+                      style: GoogleFonts.nunito(
+                        color: blackColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 14),
-                  Text(
-                    "J'ai oublié mon mot de passe",
-                    style: GoogleFonts.nunito(
-                      color: secondaryColor,
-                      fontSize: 17,
-                      fontWeight: FontWeight.w800,
+                    const SizedBox(height: 10),
+                    Text(
+                      'Content de vous revoir',
+                      style: GoogleFonts.nunito(
+                        color: greyColor,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    "OU",
-                    style: GoogleFonts.nunito(
-                      color: blackColor,
-                      fontSize: 17,
-                      fontWeight: FontWeight.w800,
+                    const SizedBox(height: 10),
+                    Image.asset(
+                      'assets/images/welcome.png',
                     ),
-                  ),
-                  const SizedBox(height: 2),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 2,
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(
+                        validator: (value) {
+                          bool emailValid = RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value!);
+
+                          if (value.isEmpty) {
+                            return "Veuillez entrer une adresse email";
+                          }
+                          if (!emailValid) {
+                            return "Veuillez entrer une adresse email valide";
+                          }
+
+                          return null;
+                        },
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(25)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: greyColor),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          hintText: 'Entrez votre email',
+                          labelText: 'Email',
+                          labelStyle:
+                              TextStyle(color: blackColor, fontSize: 20),
+                          filled: true,
+                          fillColor: whiteColor,
                         ),
-                        decoration: BoxDecoration(
-                          //borderRadius: BorderRadius.circular(30),
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: primaryColor,
-                            width: 2,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Veuillez entrer un mot de passe";
+                          } else if (value.length < 6) {
+                            "Le mot de passe doit contenir au moins 6 caractères";
+                          }
+                          return null;
+                        },
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: greyColor),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          hintText: 'Entrez votre mot de passe',
+                          labelText: 'Mot de passe',
+                          labelStyle:
+                              TextStyle(fontSize: 20, color: blackColor),
+                          filled: true,
+                          fillColor: whiteColor,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    GestureDetector(
+                      child: MaterialButton(
+                        onPressed: null,
+                        child: Container(
+                          color: primaryColor,
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          width: MediaQuery.of(context).size.width,
+                          child: Text(
+                            "Se connecter",
+                            style: GoogleFonts.nunito(
+                              color: whiteColor,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
                         ),
-                        child: Text(
-                          "f",
-                          style: GoogleFonts.nunito(
-                            color: primaryColor,
-                            fontSize: 25,
+                      ),
+                      onTap: () {},
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return const ForgotPasswordPage();
+                              }));
+                            },
+                            child: Text(
+                              "J'ai oublié mon mot de passe",
+                              style: GoogleFonts.nunito(
+                                color: secondaryColor,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      "OU",
+                      style: GoogleFonts.nunito(
+                        color: blackColor,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    const SizedBox(height: 2),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 2,
+                          ),
+                          decoration: BoxDecoration(
+                            //borderRadius: BorderRadius.circular(30),
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: primaryColor,
+                              width: 2,
+                            ),
+                          ),
+                          child: Text(
+                            "f",
+                            style: GoogleFonts.nunito(
+                              color: primaryColor,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w800,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        Container(
+                          height: 40,
+                          width: 40,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 2,
+                          ),
+                          decoration: BoxDecoration(
+                            //borderRadius: BorderRadius.circular(30),
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: primaryColor,
+                              width: 2,
+                            ),
+                          ),
+                          child: Text(
+                            "G",
+                            style: GoogleFonts.nunito(
+                              color: primaryColor,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w800,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Je n'ai pas un compte? ",
+                          style: TextStyle(
+                            color: greyColor,
                             fontWeight: FontWeight.w800,
                           ),
-                          textAlign: TextAlign.center,
                         ),
-                      ),
-                      const SizedBox(width: 20),
-                      Container(
-                        height: 40,
-                        width: 40,
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 2,
-                        ),
-                        decoration: BoxDecoration(
-                          //borderRadius: BorderRadius.circular(30),
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        child: Text(
-                          "G",
-                          style: GoogleFonts.nunito(
-                            color: primaryColor,
-                            fontSize: 25,
-                            fontWeight: FontWeight.w800,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 5),
-                  RichText(
-                    text: TextSpan(
-                        text: "Je n'ai pas un compte? ",
-                        style: TextStyle(
-                          color: greyColor,
-                          fontWeight: FontWeight.w800,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: "S'INSCRIRE",
+                        GestureDetector(
+                          onTap: () {},
+                          //widget.showRegisterPage
+                          child: Text(
+                            "S'INSCRIRE",
                             style: TextStyle(
                               color: secondaryColor,
                               fontWeight: FontWeight.w800,
                             ),
                           ),
-                        ]),
-                  ),
-                ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
